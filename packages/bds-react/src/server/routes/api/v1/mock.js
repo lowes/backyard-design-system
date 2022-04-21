3185cc2b929e0af2cfc58a10609a00e5d37be398
Bryant Derosier <bryantjderosier@gmail.com>
2022-04-21 11:43:49 -0400

    chore: remove server

--- packages/bds-react/src/server/routes/api/v1/mock.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    method: "GET",
-    path: "/api/v1/mock",
-    handler: (request, h) => h.response({ text: 'Loaded!' }).code(200)
-}
--- packages/bds-react/src/server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app')
-const mock = require('./api/v1/mock')
-const datatable = require('./api/components/datatable/example')
-
-module.exports = [
-    app,
-    mock,
-    datatable
-]
